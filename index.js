075419f61591189a868dad143f36657108939d1c
Niloufar Sabbaghian <niloufarsabbaghian@Niloufars-iMac.local>
2022-10-17 21:51:20 -0700

    question setup

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-console.log('its working')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is the Title of your project?",
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: "What is the description of your project?",
+        name: 'description',
+      },
+    {
+        type: 'input',
+        message: "What are the installation instructions?",
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: "What is the usage of this project?",
+        name: 'usage',
+     },
+     {
+       type: 'input',
+       message: "What are the contribution guidelines?",
+       name: 'contributing',
+     },
+     {
+       type: 'input',
+       message: "What are the test instructions?",
+       name: 'tests',
+     },
+     {
+       type: 'list',
+       message: "Which license do you want to use?",
+       name: 'license',
+       choices:['Apache', 'MIT', 'BSD3', 'Boost', 'Eclipse', 'GNUv3', 'Mozilla', 'Unlicense'],
+     },
+     {
+        type: 'input',
+        message: "What is your Email address?",
+        name: 'email',
+     },
+     {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'github',
+     }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    fs.writeFile('README.md', data, function (err) {
+        err ? console.log(err) : console.log("README created!")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (answers => writeToFile(generateMarkdown(answers)))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,119 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+
+  switch(answers.license) {
+    case "Apache":
+      badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case "MIT":
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case "BSD3":
+      badge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      break;
+    case "Boost":
+      badge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+      break;
+    case "Eclipse":
+      badge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+      break;
+    case "GNUv3":
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case "Mozilla":
+      badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+    case "Unlicense":
+      badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+      break;
+    default:
+      badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+
+  switch(license) {
+    case "MIT":
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "BSD":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "GPL":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+      break;
+    case "Apache":
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.html";
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSect = "";
+
+  // if a license has been selected, create License section
+  // with link to license information
+  if (license != "None") {
+    licenseSect += "## License\n"
+    licenseSect += "Please see " + renderLicenseLink(license) + " to get detailed information for this license\n";
+  }
+
+  return licenseSect;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Table of Contenets
+  1. [Desription](#Description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Video](#Video)
+  7. [License](#License)
+  8. [Questions](#Questions)
+  
+  ## 1. Description
+  ${data.description}
+
+
+  ## 2. Installation
+  ${data.installation}
+
+  ## 3. Usage
+  ${data.usage}
+
+  ## 4. Contributing
+  ${data.contributing}
+
+  ## 5. Tests
+  ${data.tests}
+
+  ## 6. Video
+
+  ## 7. License
+  ${data.license}
+  Please see [license] to get detailed information for this license.
+
+  ## 8. Questions
+  - I can be reached at ${data.email} with any additional questions.
+  - You can fine me [HERE]${data.github} on GitHub
 
 `;
 }
